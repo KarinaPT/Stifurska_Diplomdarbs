845c7fc5cbdcf2ba899e073c9c9b891ae6756b97
Karina <kstifurska@gmail.com>
2023-05-16 22:54:28 +0300

    SQL (Vaicājumi, DB)

--- SQL/kiriyena_kods.sql
+++ SQL/kiriyena_kods.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS `kiriyena_db`.`Administrators` (
   `Uzvards` VARCHAR(50) NOT NULL,
   `E_pasts` VARCHAR(100) NOT NULL,
   `T_numurs` VARCHAR(12) NULL,
-  `Loma` VARCHAR(15) NOT NULL DEFAULT 'Administrators',
+  `Loma` VARCHAR(15) NOT NULL DEFAULT 'Administrātors',
   `Parole` VARCHAR(50) NOT NULL,
   `Attela_admin` TEXT NULL,
   PRIMARY KEY (`Administrators_ID`))
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `kiriyena_db`.`Pardevejs` (
   `E_pasts_pardevejs` VARCHAR(100) NOT NULL,
   `T_numurs_pardevejs` VARCHAR(12) NULL,
   `Loma` VARCHAR(10) NOT NULL DEFAULT 'Pārdevējs',
-  `Apraksts` VARCHAR(250) NULL,
+  `Apraksts` TEXT NULL,
   `Izveidosanas_datums` DATETIME NULL,
   `Brenda_nosaukums` VARCHAR(100) NOT NULL,
   `Attela_URL` TEXT NULL,
@@ -65,6 +65,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `kiriyena_db`.`Kategorija` (
   `Kategorija_ID` INT NOT NULL AUTO_INCREMENT,
   `Nosaukums_kategorija` VARCHAR(30) NOT NULL,
+  `Kat_attela` TEXT NOT NULL,
   PRIMARY KEY (`Kategorija_ID`))
 ENGINE = InnoDB;
 
@@ -86,7 +87,7 @@ CREATE TABLE IF NOT EXISTS `kiriyena_db`.`Prece` (
   PRIMARY KEY (`Prece_ID`, `ID_Pardevejs`, `IDKapakssadala`, `ID_Kategorija`),
 --  INDEX `fk_Prece_Pārdevējs_idx` (`ID_Pardevejs` ASC) VISIBLE,
 --  INDEX `fk_Prece_K_apakssadala1_idx` (`IDKapakssadala` ASC) VISIBLE,
- -- INDEX `fk_Prece_Kategorija1_idx` (`ID_Kategorija` ASC) VISIBLE,
+--  INDEX `fk_Prece_Kategorija1_idx` (`ID_Kategorija` ASC) VISIBLE,
   CONSTRAINT `fk_Prece_Pārdevējs`
     FOREIGN KEY (`ID_Pardevejs`)
     REFERENCES `kiriyena_db`.`Pardevejs` (`Pardevejs_ID`)
@@ -108,3 +109,13 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `kiriyena_db`.`Administrators`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `kiriyena_db`;
+INSERT INTO `kiriyena_db`.`Administrators` (`Administrators_ID`, `Vards`, `Uzvards`, `E_pasts`, `T_numurs`, `Loma`, `Parole`, `Attela_admin`) VALUES (DEFAULT, 'Karina', 'Štifurska', 'kstifurska@gmail.com', '+37126459875', 'Administrātors', '21232f297a57a5a743894a0e4a801fc3', 'https://cdn.onlinewebfonts.com/svg/img_325788.png');
+
+COMMIT;
+
